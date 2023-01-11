8bbc95acf6294bb1899545140c9e9070faa4dce9
patelshivani3 <shivpatel3001@gmail.com>
2023-01-12 00:35:59 +0530

    Creating Stack

--- StackAndQueueDataStructure/Program.cs
+++ StackAndQueueDataStructure/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,23 @@ namespace StackAndQueueDataStructure
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to Stack And Queue Data Structure ");
+            Console.WriteLine("\n1:Create stack using LinkedList");        
+            int option = Convert.ToInt32(Console.ReadLine());
+            Stack stack = new Stack();
+            switch (option)        
+            {
+                case 1://UC1 Create stack
+                    stack.Push(70);
+                    stack.Push(30);
+                    stack.Push(56);
+                    Console.WriteLine("-------------------------------------");
+                    stack.Display();
+                    break;
+
+                default:
+                    Console.WriteLine("Please choose correct option");
+                    break;
+            }
             Console.ReadLine();
         }
     }
---
+++ StackAndQueueDataStructure/Stack.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueDataStructure
+{
+    internal class Stack
+    {
+        public Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        public  void Push(int newData)  //Push the data in stack
+        {
+            Node newNode = new Node(newData);
+            if (this.top != null)
+            {
+                newNode.next = this.top;
+            }
+            this.top = newNode;
+            Console.WriteLine("Node {0} pushed to stack", newData);
+        }
+        public  void Display()    //Display the data of stack
+        {
+            Node temp = this.top;
+
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}

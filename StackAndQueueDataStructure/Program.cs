d727d420ae137ff2004732a948230e415e72a7d7
patelshivani3 <shivpatel3001@gmail.com>
2023-01-12 00:10:20 +0530

    Adding Welcome Message to Stack & Queue Data structure

---
+++ StackAndQueueDataStructure/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueueDataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nWelcome to Stack And Queue Data Structure ");
+            Console.ReadLine();
+        }
+    }
+}
